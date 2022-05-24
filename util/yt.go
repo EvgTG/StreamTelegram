7206a2d9e32583f05fe9f090d2458abedcb8c4c8
Evg <fint060@gmail.com>
2022-05-24 16:03:44 +0300

    func TypeVideo

--- util/yt.go
+++ util/yt.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func GetChannelIDByUrl(url string) (string, error) {
@@ -42,4 +43,53 @@ func GetChannelIDByUrl(url string) (string, error) {
 	i2 += i1
 
 	return page[i1:i2], nil
+}
+
+// 404 video upcoming live end
+func TypeVideo(url string) (string, *time.Time, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", nil, eris.Wrap(err, "http.Get(url)")
+	}
+	defer resp.Body.Close()
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil, eris.Wrap(err, "ioutil.ReadAll()")
+	}
+
+	page := string(bs)
+
+	if strings.Contains(page, `{"iconType":"ERROR_OUTLINE"}`) {
+		return "404", nil, nil
+	}
+	if !strings.Contains(page, `<span itemprop="publication" itemscope itemtype="http://schema.org/BroadcastEvent">`) {
+		return "video", nil, nil
+	}
+
+	tmStrings := []string{`<meta itemprop="startDate" content="`, `">`}
+	iTime1 := strings.Index(page, tmStrings[0])
+	if iTime1 < 0 {
+		return "", nil, eris.New("startDate 404")
+	}
+	iTime1 += len(tmStrings[0])
+	iTime2 := strings.Index(page[iTime1:], tmStrings[1])
+	if iTime2 < 0 {
+		return "", nil, eris.New("startDate 404")
+	}
+	iTime2 += iTime1
+
+	tm, err := time.Parse(time.RFC3339, page[iTime1:iTime2])
+	if err != nil {
+		return "", nil, eris.New("startDate time.Parse()")
+	}
+
+	if strings.Contains(page, `"isUpcoming":true,`) {
+		return "upcoming", &tm, nil
+	}
+	if strings.Contains(page, `"isLive":true,`) {
+		return "live", &tm, nil
+	}
+
+	return "end", &tm, nil
 }
